91b6dd2b41dc469e33b3ff0453a4b634b24cc691
tzzz <tzzz3690@gmail.com>
2026-02-14 13:40:25 +1000

    removed images from questions

--- script.js
+++ script.js
@@ -76,13 +76,13 @@ const myQuestions = [
             let question = myQuestions[currentQuestionIndex];
             document.getElementById('question-text').innerText = question.question;
 
-            const qImage = document.getElementById('question-image');
+            /*const qImage = document.getElementById('question-image');
             if (question.image) {
                 qImage.src = question.image;
                 qImage.classList.remove('hidden');
             } else {
                 qImage.classList.add('hidden');
-            }
+            }*/
             
             const answerButtons = document.getElementById('answer-buttons');
             answerButtons.innerHTML = '';
